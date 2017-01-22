048f5ea882549192825706abda5d6819801ebde1
tkthomas27 <timothykylethomas@gmail.com>
2017-01-22 17:47:27 -0500

    karatsuba.py

---
+++ course1/karatsuba.py
@@ -0,0 +1,27 @@
+def karatsuba( num1, num2 ):
+    
+    if min(num1, num2) < 10:
+        return num1 * num2
+    
+    # lengths
+    l = len(str(num1))
+    m = int(l/2)
+    
+    # divide
+    def divide(x):
+        # // 10 ** m --> will grab the first m digits
+        # % 10 ** m --> will grab the last m digits
+        return x // 10 ** m, x % 10 ** m
+
+    x1, x0 = divide(num1)
+    y1, y0 = divide(num2)
+
+    # conquer
+    # recursively call karatsuba to reduce multiplication problem to smallest problems
+    z2 = karatsuba(x1,y1)
+    z0 = karatsuba(x0,y0)
+    z1 = karatsuba(x1+x0,y1+y0) - z2 - z0
+
+    solution = z2 * 10**(2*m) + z1 * 10**m + z0
+    return solution
+
