48cad0ac278ebe46d1117d3e4c44528bf314e7d1
tkthomas27 <timothykylethomas@gmail.com>
2017-02-16 11:31:04 -0500

    quicksort.py most sorts but a few bugs

--- course1/quicksort.py
+++ course1/quicksort.py
@@ -1,29 +1,35 @@
 
-x = [3,1,4,8,5,6,7]
+def partition(A,l,r):
+    p = A[l]
+    i = l+1
+    j = l+1
+    for j in range (j,r):
+        if A[j] < p:
+            A[j], A[i] = A[i], A[j]
+            i = i+1
+    A[l], A[i-1] = A[i-1], A[l]
+    
+    return A, i
+
+x = [3,8,2,5,1,4,7,6]
+
+aa = partition(x,0,len(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # pivot element 1
-
-def quicksort(A, n):
-    
-    if n=1: return A
-    
-    p = ChoosePivot(A,n)
-    
-    partition(A,l,r):
-        p = A[l]
-        i = l+1
-        j = l+1
-        for j in range (j,r):
-            if A[j] < p:
-                A[j] = A[i]
-                i = i+1
-        A[l] = A[i-1]
-    
-    Recursively Sort 1st part
-    
-    Recursivley Sort 2nd part
-
-
 
 def partition(A,l,r):
     p = A[l]
@@ -35,11 +41,23 @@ def partition(A,l,r):
             i = i+1
     A[l], A[i-1] = A[i-1], A[l]
     
-    return A
+    return i
 
-x = [3,8,2,5,1,4,7,6]
+def quicksort(A,l,r):
+    
+    if l<r:
+    
+        # parittion
+        i = partition(A,l,r)
+        
+        quicksort(A,l,i-1)
+        quicksort(A,i+1,r)
 
-partition(x,0,len(x))
+
+x = [54,26,93,17,77,31,44,55,20]
+quicksort(x,0,len(x))
+x
+
 
 
 
